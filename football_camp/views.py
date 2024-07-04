71a02157ddd949668b6a83d360ff288e59772cbb
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-07-04 19:43:51 +0000

    Update sittings.py with csrf trusted origins

--- football_camp/views.py
+++ football_camp/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('Hello football players')
+
+def home(request):
+    return render(request, 'football_camp/base.html')
