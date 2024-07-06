c894664d5d4b50bfa4e462ebecccdd0bef32e9a3
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-07-06 20:19:20 +0000

    Update football_camp/urls

--- football_camp/urls.py
+++ football_camp/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('services/', views.service_list, name='service_list'),
     path('book_service/', views.book_service, name='book_service'),
+    path('admin_create_service/', views.admin_create_service, name='admin_create_service'),
+    path('admin_manage_services/', views.admin_manage_services, name='admin_manage_services'),
+    path('view_training_schedule/', views.view_training_schedule, name='view_training_schedule'),
+    path('manage_players/', views.manage_players, name='manage_players'),
 ]
