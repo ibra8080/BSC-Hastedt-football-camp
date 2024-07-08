8ee35d8828f9228fb7ec0e0a73a3960a6b47b087
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-07-08 07:50:12 +0000

    Create register and login .html

--- my_project/settings.py
+++ my_project/settings.py
@@ -129,3 +129,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Authentication settings
+LOGIN_REDIRECT_URL = '/'
