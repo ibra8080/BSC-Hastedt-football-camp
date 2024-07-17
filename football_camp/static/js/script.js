843149c1cda8f6192a2bb42157c00d050986221e
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-07-17 22:02:47 +0000

    Enhance responsiveness forms style

--- football_camp/static/js/script.js
+++ football_camp/static/js/script.js
@@ -1,17 +1,18 @@
 console.log("It works!");
 
 document.addEventListener('DOMContentLoaded', function() {
-    var showPasswordCheckbox = document.getElementById('show_password');
-    if (showPasswordCheckbox) {
-        showPasswordCheckbox.addEventListener('change', function() {
-            var passwordFields = document.querySelectorAll('input[type="password"]');
-            passwordFields.forEach(field => {
-                if (this.checked) {
-                    field.type = 'text';
-                } else {
-                    field.type = 'password';
-                }
-            });
+    const showPasswordCheckbox = document.getElementById('show_password');
+    const passwordFields = document.querySelectorAll('input[type="password"]');
+
+    showPasswordCheckbox.addEventListener('change', function() {
+        const type = this.checked ? 'text' : 'password';
+        passwordFields.forEach(field => {
+            field.setAttribute('type', type);
         });
-    }
+    });
+
+    const telephoneField = document.querySelector('input[name="telephone"]');
+    telephoneField.addEventListener('input', function() {
+        this.value = this.value.replace(/\D/g, '');
+    });
 });
