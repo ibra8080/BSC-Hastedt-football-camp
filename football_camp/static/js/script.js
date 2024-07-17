2856b25a5c99a2cfc908ebfe2a66c68ba6adefac
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-07-17 20:18:57 +0000

    Update script.js with show password function

--- football_camp/static/js/script.js
+++ football_camp/static/js/script.js
@@ -1,1 +1,17 @@
 console.log("It works!");
+
+document.addEventListener('DOMContentLoaded', function() {
+    var showPasswordCheckbox = document.getElementById('show_password');
+    if (showPasswordCheckbox) {
+        showPasswordCheckbox.addEventListener('change', function() {
+            var passwordFields = document.querySelectorAll('input[type="password"]');
+            passwordFields.forEach(field => {
+                if (this.checked) {
+                    field.type = 'text';
+                } else {
+                    field.type = 'password';
+                }
+            });
+        });
+    }
+});
