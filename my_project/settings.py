06aed045c9036db3a7bce77f7082d09a9b6c1cb1
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-07-21 14:37:50 +0000

    Add a new servises images

--- my_project/settings.py
+++ my_project/settings.py
@@ -16,6 +16,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+# DEBUG = True
 
 ALLOWED_HOSTS = [
     '.codeinstitute-ide.net',
