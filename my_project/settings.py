3705b9b10fe0019c69685d3082d75e7b3301adb6
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-07-21 15:12:25 +0000

    Ensure media files are served in production

--- my_project/settings.py
+++ my_project/settings.py
@@ -124,6 +124,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'football_camp/static')]
 
+# https://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
--- my_project/urls.py
+++ my_project/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Always serve media files, regardless of DEBUG setting
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = football_views.custom_404
