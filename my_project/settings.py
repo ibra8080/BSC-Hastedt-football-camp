e3cee85946b8969c1ece01c1627db6d491969ec1
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-07-22 20:13:23 +0000

    Update settings with debug

--- my_project/settings.py
+++ my_project/settings.py
@@ -9,7 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = [
     '.codeinstitute-ide.net',
